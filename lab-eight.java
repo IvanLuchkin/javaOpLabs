56dcaedf10afebf21b40c553fb8ab4bc74c2878e
Ivan <id.luchkin@gmail.com>
2018-12-13 23:49:11 +0200

    Update lab-eight.java

--- lab-eight.java
+++ lab-eight.java
@@ -1,6 +1,49 @@
 import java.util.*;
 
 public class Lab8 {
+	
+	public static int[][] fillMatrix(int[][] matrix){
+		for (int i = 0; i < matrix.length; i++) {						// Filling the matrix with numbers
+			for (int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = (int)(Math.random() * 200 - 100);
+				System.out.print(matrix[i][j] + "\t"); 
+			}
+			System.out.println();
+		}
+		return matrix;
+	}
+	
+	public static double[] createNewMatrix(int[][] matrix) {
+		double [] X = new double [matrix.length];						// Calculating the components of the resulting array (vector) X[]
+		for (int i = 0; i < matrix.length; i++) {
+			double sum = 0;
+			for (int j = 0; j < matrix[i].length; j++) {
+				sum += matrix[i][j];
+			}		
+			X[i] = sum / matrix[i].length;
+		}
+		return X;
+	}
+	
+	public static double[] matrixSort(double[] matrix) {
+		int last = matrix.length;										// Sorting X[]
+		for (boolean sorted = last == 0; !sorted; --last) {
+		    sorted = true;
+		    for (int i = 1; i < last; ++i) {
+		        if (matrix[i-1] < matrix[i]) {                
+		            sorted = false;
+		            double tmp = matrix[i - 1];
+		            matrix[i-1] = matrix[i];
+		            matrix[i] = tmp;
+		        }
+		    }               
+		}
+		
+		for (int i = 0; i < matrix.length; i++) {
+			System.out.print(matrix[i] + " ");
+		}
+		return matrix;
+	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);				// Matrix size input
@@ -10,41 +53,9 @@ public class Lab8 {
 		int m = in.nextInt();
 		int [][] matrix = new int [n][m];
 		
-		for (int i = 0; i < n; i++) {						// Filling the matrix with numbers
-			for (int j = 0; j < m; j++) {
-				matrix[i][j] = (int)(Math.random() * 200 - 100);
-				System.out.print(matrix[i][j] + "\t"); 
-			}
-			System.out.println();
-		}
+		fillMatrix(matrix);
 		
-		double [] X = new double [n];						// Calculating the components of the resulting array (vector) X[]
-		for (int i = 0; i < n; i++) {
-			double sum = 0;
-			for (int j = 0; j < m; j++) {
-				sum += matrix[i][j];
-			}		
-			X[i] = sum / m;
-		}
-		
-		int last = n;										// Sorting X[]
-		for (boolean sorted = last == 0; !sorted; --last) {
-		    sorted = true;
-		    for (int i = 1; i < last; ++i) {
-		        if (X[i-1] < X[i]) {                
-		            sorted = false;
-		            double tmp = X[i - 1];
-		            X[i-1] = X[i];
-		            X[i] = tmp;
-		        }
-		    }               
-		}
-		
-		System.out.print("X( ");							// Results output
-		for (int i = 0; i < n; i++) {
-			System.out.print(X[i] + " ");
-		}
-		System.out.print(" )");
+		matrixSort(createNewMatrix(matrix));
 		
 	}
 
